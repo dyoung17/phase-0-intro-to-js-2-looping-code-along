8468e9393be55f6523699df8bfe581d4881600f1
Daven Young <davenyoung@msn.com>
2022-04-08 13:22:05 -0600

    commit tree 9b450ce82219ed551489893145b1c92fe191aaf1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+let cards = ["Guadalupe", "Ollie", "Aki"];
+
+
+
+function writeCards(cards){
+    for (let i = 0; i > cards.length; i++){
+        console.log(`Thank you, ${cards[i]} for the wonderful surprise gift`);
+    debugger;
+    }
+
+    return [cards];
+}
